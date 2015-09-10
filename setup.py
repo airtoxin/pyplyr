0d3513a1f1e288cf5049f0e3bd08f357b914d037
airtoxin <airtoxin@icloud.com>
2015-09-11 00:26:28 +0900

    Add setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="pyplyr",
+    version="0.0.1",
+    description="SQL like method chains for pandas DataFrame.",
+    author="airtoxin",
+    author_email="airtoxin@icloud.com",
+    url="https://github.com/airtoxin/pyplyr",
+    py_modules=["pyplyr"],
+    include_package_data=True,
+    install_requires=[
+        "pandas"
+    ],
+    tests_require=[
+        "nose"
+    ],
+    license="MIT",
+    keywords="",
+    zip_safe=False,
+    classifiers=[]
+)
