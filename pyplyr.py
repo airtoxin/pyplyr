b69630368651416f7338ef7e980be408a5386e31
airtoxin <airtoxin@icloud.com>
2015-09-11 01:06:10 +0900

    dplyr like api

--- pyplyr.py
+++ pyplyr.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+from pandas import Series, DataFrame
+
 class Pyplyr(object):
     """Pyplyr"""
     def __init__(self, df):
         super(Pyplyr, self).__init__()
         self.__df = df
 
-    def select(self, names):
-        """SQL: SELECT clause"""
-        if isinstance(names, str): names = [names]
-        return Pyplyr(self.__df[names])
+    def __str__(self):
+        return self.__df.__str__()
+
+    def select(self, *cols):
+        return Pyplyr(self.__df[list(cols)])
+
+    def filter(self, operator="or", **kwargs):
+        if operator not in ("or", "and"): raise ValueError()
+
+        total_mask = None
+        for col, func in kwargs.items():
+            mask = self.__df[col].map(func)
+            if total_mask is None:
+                total_mask = mask
+                continue
+            if operator is "or":
+                total_mask = mask | total_mask
+            elif operator is "and":
+                total_mask = mask & total_mask
+
+        return Pyplyr(self.__df[total_mask])
+
+    def summarize(self, **kwargs):
+        series_list = []
+        for new_col, assets in kwargs.items():
+            target_col, func = assets
+            val = func(self.__df[target_col])
+            series = Series([val], name=new_col)
+            series_list.append(series)
+        return Pyplyr(pd.concat(series_list, axis=1))
 
     def data(self):
         """Called at end of method chains"""
--- tests/pyplyr_test.py
+++ tests/pyplyr_test.py
@@ -1,42 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from unittest import TestCase
-from nose.tools import ok_, eq_
-
 from pyplyr import Pyplyr
-from pandas import DataFrame
+from pandas import Series, DataFrame
 
-class PyplyrPublicMethodsTestCase(TestCase):
-    def setUp(self):
+class TestPyplyr(object):
+    def setup(self):
+        self.columns = ["label1", "col0", "col1", "col2", "col3", "col4"]
         self.df = DataFrame([
-            [0,  1,  2,  3,  4],
-            [5,  6,  7,  8,  9],
-            [10, 11, 12, 13, 14],
-            [15, 16, 17, 18, 19],
-            [20, 21, 22, 23, 24]
-        ], columns=["col0", "col1", "col2", "col3", "col4"])
+            ["hoge", 0,  1,  2,  3,  4],
+            ["fuga", 5,  6,  7,  8,  9],
+            ["hoge", 10, 11, 12, 13, 14],
+            ["fuga", 15, 16, 17, 18, 19],
+            ["hoge", 20, 21, 22, 23, 24]
+        ], columns=self.columns)
         self.pyplyr = Pyplyr(self.df)
-    def tearDown(self):
+
+    def teardown(self):
+        self.columns = None
         self.df = None
         self.pyplyr = None
 
     def test_constructor(self):
-        ok_(self.pyplyr._Pyplyr__df is self.df)
+        assert self.pyplyr._Pyplyr__df is self.df
 
-    def test_select_with_single_col(self):
-        sel_list = "col1"
-        selected = self.pyplyr.select(sel_list)._Pyplyr__df
-        expected = self.df[[sel_list]]
-        is_ok = (selected == expected).all().all()
-        ok_(is_ok)
-
-    def test_select_with_multi_col(self):
-        sel_list = ["col1", "col4"]
-        selected = self.pyplyr.select(sel_list)._Pyplyr__df
-        expected = self.df[sel_list]
-        is_ok = (selected == expected).all().all()
-        ok_(is_ok)
+    def test_select(self):
+        cols = ["col1", "label1"]
+        result = self.pyplyr.select(*cols)
+        df = result._Pyplyr__df
+        assert isinstance(result, Pyplyr)
+        assert list(df.columns) == cols
+        assert (df == self.df[cols]).all().all()
 
     def test_data(self):
-        ok_(self.pyplyr.data() is self.df)
+        assert (self.pyplyr.data() == self.df).all().all()
+        assert (self.pyplyr.data() is self.df)
+
+    def test_filter_with_default(self):
+        result = self.pyplyr.filter(
+            label1=lambda x: x == "hoge",
+            col1=lambda x: x > 10
+        )
+        df = result._Pyplyr__df
+        assert isinstance(result, Pyplyr)
+        assert set(df.columns) == set(self.df.columns)
+        assert (df == self.df[Series([True, False, True, True, True])]).all().all()
+
+    def test_filter_with_or_operator(self):
+        result = self.pyplyr.filter(
+            operator="or",
+            label1=lambda x: x == "hoge",
+            col1=lambda x: x > 10
+        )
+        df = result._Pyplyr__df
+        assert isinstance(result, Pyplyr)
+        assert set(df.columns) == set(self.df.columns)
+        assert (df == self.df[Series([True, False, True, True, True])]).all().all()
+
+    def test_filter_with_and_operator(self):
+        result = self.pyplyr.filter(
+            operator="and",
+            label1=lambda x: x == "hoge",
+            col1=lambda x: x > 10
+        )
+        df = result._Pyplyr__df
+        assert isinstance(result, Pyplyr)
+        assert set(df.columns) == set(self.df.columns)
+        assert (df == self.df[Series([False, False, True, False, True])]).all().all()
+
+    def test_summarize(self):
+        result = self.pyplyr.summarize(
+            c0sum=("col0", sum),
+            c1cnt=("col1", len),
+            hogecnt=("label1", lambda iter: len([i for i in iter if i == "hoge"]))
+        )
+        df = result._Pyplyr__df
+        expected = DataFrame([[50, 5, 3]], columns=["c0sum", "c1cnt", "hogecnt"])
+        assert isinstance(result, Pyplyr)
+        assert set(df.columns) == set(["c0sum", "c1cnt", "hogecnt"])
+        assert len(df) == 1
+        assert df.c0sum[0] == 50
+        assert df.c1cnt[0] == 5
+        assert df.hogecnt[0] == 3
